e360ebd5b49c077a71b73c4686356999a3619cee
Jiaxin <jl2458@rutgers.edu>
2020-08-06 19:16:05 -0400

    hi

--- async.js
+++ async.js
@@ -1,6 +1,37 @@
-window.onliad = function () {
-  $.get('async/tweets.json', function (data) {
-    console.log(data);
+window.onload = function () {
+  $.ajax({
+    type: 'GET',
+    url: 'tweets.json',
+    success: function (data) {
+      console.log(data);
+      // query friends.json
+      window.onload = function () {
+        $.ajax({
+          type: 'GET',
+          url: 'friends.json',
+          success: function (data) {
+            console.log(data);
+            window.onload = function () {
+              $.ajax({
+                type: 'GET',
+                url: 'videos.json',
+                success: function (data) {
+                  console.log(data);
+                },
+                error: function (jqXHR, textStatus, error) {
+                  console.log(error);
+                },
+              });
+            };
+          },
+          error: function (jqXHR, textStatus, error) {
+            console.log(error);
+          },
+        });
+      };
+    },
+    error: function (jqXHR, textStatus, error) {
+      console.log(error);
+    },
   });
-  console.log('test');
 };
